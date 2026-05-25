532dc409522ca88e16f478359755e16ead8aa795
Alexander Petranov <shasho68.5@gmail.com>
2026-05-25 14:48:14 +0300

    Install tailwind css and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import { Button } from './components/ui/button'
 
 
 function App() {
 
   return (
-    <div>
-      
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <h1 className='text-5xl font-bold'>🥷WorkoutNinja</h1>
+      <Button>+ Create Workout</Button>
     </div>
   )
 }
