6de0b2de33096829caff054d8df8c5206452cae4
Alexander Petranov <shasho68.5@gmail.com>
2026-05-25 15:00:33 +0300

    Add create workout button and pop up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import { Button } from './components/ui/button'
+import CreateWorkout from './components/CreateWorkout'
 
 
 function App() {
-
+  
   return (
     <div className='flex flex-col justify-center items-center min-h-screen'>
-      <h1 className='text-5xl font-bold'>🥷WorkoutNinja</h1>
-      <Button>+ Create Workout</Button>
+      <h1 className='text-3xl md:text-5xl font-bold'>🥷WorkoutNinja</h1>
+      <CreateWorkout />
     </div>
   )
 }
