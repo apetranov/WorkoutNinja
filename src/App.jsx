2a244313fe6f3ec159dcb698d9965b1455bfd837
Alexander Petranov <shasho68.5@gmail.com>
2026-05-26 12:50:03 +0300

    Add finish set feature when workout started

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Button } from "./components/ui/button";
 import CreateWorkout from "./components/CreateWorkout";
 import AddExerise from "./components/AddExerise";
 import DeleteExercise from "./components/DeleteExercise";
+import Workout from "./components/Workout";
+import FinishSet from "./components/FinishSet";
 
 function App() {
   const [dayOfWeek, setDayOfWeek] = useState("");
@@ -12,10 +14,11 @@ function App() {
   const [exercises, setExercises] = useState([]);
   const [exercise, setExercise] = useState({});
   const [exerciseName, setExerciseName] = useState("");
-  const [sets, setSets] = useState(0)
-  const [reps, setReps] = useState(0)
+  const [sets, setSets] = useState(0);
+  const [reps, setReps] = useState(0);
   const [finishedSets, setFinishedSets] = useState(0);
   const [exerciseId, setExerciseId] = useState(0);
+  const [workoutRunning, setWorkoutRunning] = useState(false);
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
@@ -34,50 +37,67 @@ function App() {
           />
         </div>
       )}
-      {Object.keys(workout).length > 0 && <div className="flex w-full md:w-1/2 space-y-2 outline p-10 shadow-2xl flex-col justify-center items-center">
+      {Object.keys(workout).length > 0 && (
+        <div className="flex w-full md:w-1/2 space-y-2 outline p-10 shadow-2xl flex-col justify-center items-center">
           <h1 className="font-bold text-center  text-4xl">📅{workout.day}</h1>
-          <h1 className="text-3xl text-center font-bold text-gray-400">💪🏽{workout.muscles}</h1>
-          <h1 className="text-xl text-center font-bold text-gray-500">⏳{workout.time} minutes</h1>
+          <h1 className="text-3xl text-center font-bold text-gray-400">
+            💪🏽{workout.muscles}
+          </h1>
+          <h1 className="text-xl text-center font-bold text-gray-500">
+            ⏳{workout.time} minutes
+          </h1>
           <div className="w-full flex flex-col justify-center items-center space-y-5">
             {workout.exercises.map((ex) => (
-              <div key={ex.exerciseId} className="grid grid-cols-4 w-full justify-center items-center text-center">
+              <div
+                key={ex.exerciseId}
+                className={!workoutRunning ? 
+                  `grid grid-cols-4 w-full justify-center items-center text-center` 
+                  : `grid grid-cols-3 w-full justify-center items-center text-center` 
+                }
+              >
                 <h2 className="flex flex-col">
                   <p className="font-bold">Exercise</p>
                   <p>{ex.exerciseName}</p>
                 </h2>
                 <h2 className="flex flex-col">
                   <p className="font-bold">Sets</p>
-                  <p>{ex.sets}</p>
+                  {/* {workoutRunning ? <p>{ex.finishedSets}/{ex.sets}</p> : <p>{ex.sets}</p> } */}
+                  {ex.finishedSets}/{ex.sets}
+                  {workoutRunning && ex.finishedSets < ex.sets && <FinishSet workout={workout} setWorkout={setWorkout} exId={ex.exerciseId} />}
                 </h2>
                 <h2 className="flex flex-col">
                   <p className="font-bold">Reps</p>
                   <p>{ex.reps}</p>
                 </h2>
-                <DeleteExercise
+                {!workoutRunning && <DeleteExercise
                   workout={workout}
                   setWorkout={setWorkout}
                   delId={ex.exerciseId}
-                />
+                />}
               </div>
             ))}
-            <AddExerise
-              exercises={exercises}
-              setExercises={setExercises}
-              exercise={exercise}
-              setExercise={setExercise}
-              exerciseName={exerciseName}
-              setExerciseName={setExerciseName}
-              sets={sets}
-              setSets={setSets}
-              reps={reps}
-              setReps={setReps}
-              workout={workout}
-              setWorkout={setWorkout}
-              exerciseId={exerciseId}
-              setExerciseId={setExerciseId}
-            />
+            {!workoutRunning && (
+              <AddExerise
+                exercises={exercises}
+                setExercises={setExercises}
+                exercise={exercise}
+                setExercise={setExercise}
+                exerciseName={exerciseName}
+                setExerciseName={setExerciseName}
+                sets={sets}
+                setSets={setSets}
+                reps={reps}
+                setReps={setReps}
+                workout={workout}
+                setWorkout={setWorkout}
+                exerciseId={exerciseId}
+                setExerciseId={setExerciseId}
+              />
+            )}
+            {workout.exercises.length > 0 && <Workout workoutRunning={workoutRunning} setWorkoutRunning={setWorkoutRunning} />}
           </div>
-        </div>}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Workout.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Button } from "../components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "../components/ui/field"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
+import { useState } from "react"
+import { useTimer } from 'react-timer-hook';
+
+export default function Workout({workoutRunning, setWorkoutRunning}) {
+
+    const startWorkout = () => {
+        setWorkoutRunning(true);
+    }
+
+  return (
+      <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button className="bg-green-600 text-white cursor-pointer" variant="outline">{workoutRunning ? "Finish Workout" : "Start Workout"}</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle className="font-bold">Are you ready?!</DialogTitle>
+            <DialogDescription>
+              You cannont delete or add exercises during the workout
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <DialogClose asChild>
+                <Button onClick={startWorkout} type="submit">🔥Let's go!</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  )
+}
